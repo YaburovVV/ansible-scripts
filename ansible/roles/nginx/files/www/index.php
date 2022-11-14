39f17692ac4ee308bca08558a6bfd1c363341821
Vladimir Yaburov <vladimir.yaburov@gmail.com>
2022-11-14 23:34:18 +0300

    added printing nginx address

--- ansible/roles/nginx/files/www/index.php
+++ ansible/roles/nginx/files/www/index.php
@@ -4,7 +4,7 @@
  </head>
  <body>
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
- <h1 style='text-align: center; padding: 25px 0 10px'>Hello World</h1>
+ <h1 style='text-align: center; padding: 25px 0 10px'>Hello Ksu</h1>
  <h2 id="runningTime" style='text-align: center; padding: 10px 0 25px'></h2>
  <script type="text/javascript">
  $(document).ready(function() {
